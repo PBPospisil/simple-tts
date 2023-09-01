b6d1f53c9082bb1dfb0391de98d1c2df740895f1
Pat Pospisil <patpospisil@Pats-MacBook-Pro.local>
2023-09-01 15:20:47 -0700

    basic skeleton for main page and impl in App

--- App.tsx
+++ App.tsx
@@ -17,6 +17,8 @@ import {
   View,
 } from 'react-native';
 
+import TextToSpeechScreen from './src/pages/TextToSpeechScreen'
+
 import {
   Colors,
   DebugInstructions,
@@ -63,35 +65,8 @@ function App(): JSX.Element {
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+    <SafeAreaView>
+      <TextToSpeechScreen></TextToSpeechScreen>
     </SafeAreaView>
   );
 }

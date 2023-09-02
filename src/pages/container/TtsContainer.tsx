40ce023ea555f30d6893dcca1fb931d4aca4d639
Pat Pospisil <patpospisil@Pats-MacBook-Pro.local>
2023-09-02 15:42:55 -0700

    moved cb interfaces to types

--- src/pages/container/TtsContainer.tsx
+++ src/pages/container/TtsContainer.tsx
@@ -1,14 +1,6 @@
 import React from "react"
 import TtsPresenter from "../presenter/TtsPresenter"
 
-export interface SliderCallback {
-    (value: number): void
-}
-
-export interface TextInputCallback {
-    (text: string): void
-}
-
 const TtsContainer = () =>
 {
     return (

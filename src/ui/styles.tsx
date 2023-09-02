e5af490de84dd7590cda15a5e72f024cac83102a
Pat Pospisil <patpospisil@Pats-MacBook-Pro.local>
2023-09-02 11:28:51 -0700

    styles

---
+++ src/ui/styles.tsx
@@ -0,0 +1,46 @@
+import { StyleSheet } from "react-native"
+
+export const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      flexDirection: 'column',
+      padding: 5,
+    },
+    titleText: {
+      fontSize: 22,
+      textAlign: 'center',
+      fontWeight: 'bold',
+    },
+    buttonStyle: {
+      justifyContent: 'center',
+      marginTop: 15,
+      padding: 10,
+      backgroundColor: '#8ad24e',
+    },
+    buttonTextStyle: {
+      color: '#fff',
+      textAlign: 'center',
+    },
+    sliderContainer: {
+      flexDirection: 'row',
+      justifyContent: 'center',
+      alignItems: 'center',
+      width: 300,
+      padding: 5,
+    },
+    sliderLabel: {
+      textAlign: 'center',
+      marginRight: 20,
+    },
+    slider: {
+      flex: 1,
+    },
+    textInput: {
+      borderColor: 'gray',
+      borderWidth: 1,
+      color: 'black',
+      width: '100%',
+      textAlign: 'center',
+      height: 40,
+    },
+  });

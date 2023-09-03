b827da7552ba82f710d5d3f3f8e44e5cdfa42953
Pat Pospisil <patpospisil@Pats-MacBook-Pro.local>
2023-09-02 17:20:33 -0700

    prop name changes

--- src/components/VoicePitchSlider.tsx
+++ src/components/VoicePitchSlider.tsx
@@ -2,28 +2,23 @@ import React from "react"
 import { View, Text } from 'react-native'
 import Slider from '@react-native-community/slider'
 import { styles } from "../ui/styles"
-import { SliderCallback } from "../pages/container/TtsContainer"
-
-type PitchSliderProps = {
-    pitch: number;
-    setVoicePitch: SliderCallback;
-}
+import { PitchSliderProps } from "../types"
 
 const VoicePitchSlider = (props: PitchSliderProps) =>
 {
-    let { pitch, setVoicePitch } = props
+    let { voicePitch, onSpeechPitchChange } = props
 
     return (
         <View style={styles.sliderContainer}>
           <Text style={styles.sliderLabel}>
-            {`Pitch: ${pitch.toFixed(2)}`}
+            {`Pitch: ${voicePitch.toFixed(2)}`}
           </Text>
           <Slider
             style={styles.slider}
             minimumValue={0.5}
             maximumValue={2}
-            value={pitch}
-            onSlidingComplete={setVoicePitch}
+            value={voicePitch}
+            onSlidingComplete={onSpeechPitchChange}
           />
         </View>
     )
--- src/components/VoiceSpeedSlider.tsx
+++ src/components/VoiceSpeedSlider.tsx
@@ -1,29 +1,24 @@
 import React from "react"
 import { View, Text } from "react-native"
 import Slider from '@react-native-community/slider'
-import { SliderCallback } from "../pages/container/TtsContainer"
 import { styles } from "../ui/styles"
-
-type SpeedSliderProps = {
-    speed: number;
-    setVoiceSpeed: SliderCallback;
-}
+import { SpeedSliderProps } from "../types"
 
 const VoiceSpeedSlider = (props: SpeedSliderProps) => 
 {
-    let { speed, setVoiceSpeed } = props
+    let { voiceSpeed, onSpeechSpeedChange } = props
 
     return (
         <View style={styles.sliderContainer}>
           <Text style={styles.sliderLabel}>
-            {`Speed: ${speed.toFixed(2)}`}
+            {`Speed: ${voiceSpeed.toFixed(2)}`}
           </Text>
           <Slider
             style={styles.slider}
             minimumValue={0.01}
             maximumValue={0.99}
-            value={speed}
-            onSlidingComplete={setVoiceSpeed}
+            value={voiceSpeed}
+            onSlidingComplete={onSpeechSpeedChange}
           />
         </View>
     )

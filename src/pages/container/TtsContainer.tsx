14c563214a023269dd60873c47a81fb7b36a2b4e
Pat Pospisil <patpospisil@Pats-MacBook-Pro.local>
2023-09-02 17:14:53 -0700

    tts state

--- src/pages/container/TtsContainer.tsx
+++ src/pages/container/TtsContainer.tsx
@@ -1,10 +1,33 @@
-import React from "react"
+import React, { useState } from "react"
 import TtsPresenter from "../presenter/TtsPresenter"
+import { ReadTextCallback, SliderCallback, TtsStatus, VoiceSelectCallback } from "../../types"
 
 const TtsContainer = () =>
 {
+    const [text, setText] = useState('Enter Text')
+    const [ttsStatus, setTtsStatus] = useState(TtsStatus.Initializing)
+    const [voiceSpeed, setVoiceSpeed] = useState(0.5)
+    const [voicePitch, setVoicePitch] = useState(1)
+    const [voices, setVoices] = useState([])
+    const [selectedVoice, setSelectedVoice] = useState(null)
+
+    const initTts = async () => {}
+    
+    const onReadText: ReadTextCallback = async () => {}
+
+    const onSpeechSpeedChange: SliderCallback = async (value) => {}
+    
+    const onSpeechPitchChange: SliderCallback = async (value) => {}
+
+    const onVoicePress: VoiceSelectCallback = async (voice) => {}
+
     return (
-        <TtsPresenter></TtsPresenter>
+        <TtsPresenter
+            textInputProps={{ text, setText }}
+            readTextButtonProps={{ ttsStatus, onReadText }}
+            voiceSpeedSliderProps={{ voiceSpeed, onSpeechSpeedChange }}
+            voicePitchSliderProps={{ voicePitch, onSpeechPitchChange }}
+            selectionListProps={{ voices, selectedVoice, onVoicePress }} />
     )
 }
 

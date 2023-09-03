49aa289e0a20814dc4e2b20029996527dd79efe3
Pat Pospisil <patpospisil@Pats-MacBook-Pro.local>
2023-09-02 20:31:16 -0700

    container cbs functionality added

--- src/pages/container/TtsContainer.tsx
+++ src/pages/container/TtsContainer.tsx
@@ -15,15 +15,16 @@ import { addCancelEventListener,
          setDefaultVoice,
          startVoiceRead,
          stopVoiceRead } from "../../services/tts"
+import { Voice } from "react-native-tts"
 
 const TtsContainer = () =>
 {
     const [text, setText] = useState('Enter Text')
-    const [ttsStatus, setTtsStatus] = useState(TtsStatus.Initializing)
-    const [voiceSpeed, setVoiceSpeed] = useState(0.5)
-    const [voicePitch, setVoicePitch] = useState(1)
-    const [voices, setVoices] = useState([])
-    const [selectedVoice, setSelectedVoice] = useState(null)
+    const [ttsStatus, setTtsStatus] = useState<TtsStatus>(TtsStatus.Initializing)
+    const [voiceSpeed, setVoiceSpeed] = useState<number>(0.5)
+    const [voicePitch, setVoicePitch] = useState<number>(1)
+    const [voices, setVoices] = useState<Voice[]>([])
+    const [selectedVoice, setSelectedVoice] = useState<string>('')
 
     useEffect(() => {
         addStartEventListener(setTtsStatus)
@@ -40,15 +41,57 @@ const TtsContainer = () =>
             removeCancelEventListener(setTtsStatus)
         }}, [])
 
-    const initTts = async () => {}
+    const initTts = async () =>
+    {
+        const voices: Voice[] = await getVoices()
+        const availableVoices: Voice[] = voices
+          .filter((v) => !v.networkConnectionRequired && !v.notInstalled)
+          .map((v) => {
+            return {id: v.id, name: v.name, language: v.language} as Voice
+          })
+        let selectedVoice: string = '';
+        if (voices && voices.length > 0) {
+          selectedVoice = voices[0].id
+          try {
+            await setDefaultLanguage(voices[0].language)
+          } catch (err) {
+            console.log(`setDefaultLanguage error `, err)
+          }
+          await setDefaultVoice(voices[0].id)
+          setVoices(availableVoices)
+          setSelectedVoice(selectedVoice) 
+        }
+        setTtsStatus(TtsStatus.Initialized)
+    }
     
-    const onReadText: ReadTextCallback = async () => {}
+    const onReadText: ReadTextCallback = async () =>
+    {
+        stopVoiceRead()
+        startVoiceRead(text)
+    }
 
-    const onSpeechSpeedChange: SliderCallback = async (value) => {}
+    const onSpeechSpeedChange: SliderCallback = async (speed) =>
+    {
+        await setDefaultSpeed(speed)
+        setVoiceSpeed(speed)
+    }
     
-    const onSpeechPitchChange: SliderCallback = async (value) => {}
+    const onSpeechPitchChange: SliderCallback = async (pitch) =>
+    {
+        await setDefaultPitch(pitch)
+        setVoicePitch(pitch)
+    }
 
-    const onVoicePress: VoiceSelectCallback = async (voice) => {}
+    const onVoicePress: VoiceSelectCallback = async (voice) =>
+    {
+        try {
+            await setDefaultLanguage(voice.language)
+          } catch (err) {
+            console.log(`setDefaultLanguage error `, err);
+          }
+          await setDefaultVoice(voice.id);
+          setSelectedVoice(voice.id);
+    }
 
     return (
         <TtsPresenter
